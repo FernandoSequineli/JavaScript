88734e100054a4d36ee4a080fd66f8dba9c6f469
FernandoSequineli <sequineli.fernando@gmail.com>
2021-01-29 11:36:10 +0000

    update

---
+++ function2.js
@@ -0,0 +1,15 @@
+let dobro = function (a) {
+    return 2 * a
+}
+
+
+dobro1 = (a) => {
+    return 2 * a 
+}
+
+dobro2 = a => 2 * a   // retorno implicito
+
+console.log(dobro2(9))
+
+ola = () => 'Ola'
+console.log(ola())
---
+++ this.js
@@ -0,0 +1,14 @@
+const pessoa = {
+    saudacao: 'Bom dia',
+    falar() {
+        //sem o this = is not defined 
+        console.log(this.saudacao)
+    }
+}
+
+//pessoa.falar()
+const falar2 = pessoa.falar
+falar2()
+// a funcao BIND "resolve" o this do obj -pessoa-
+const falar = pessoa.falar.bind(pessoa) 
+falar()
