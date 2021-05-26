722b38509f069e4888343d2ff3a5a5ebe3b57e6b
FernandoSequineli <sequineli.fernando@gmail.com>
2021-05-26 08:35:46 +0100

    update

---
+++ arrayFilter.js
@@ -0,0 +1,12 @@
+const pessoas = [
+  {nome: 'Luiz', idade: 62},
+  {nome: 'Pedro', idade:33},
+  { nome: 'Fabio', idade: 55},
+  { nome: 'Marcia', idade: 73 },
+  { nome: 'Joao', idade: 22 },
+]
+
+const nomeMaiorQueCinco = pessoas.filter(obj => obj.nome.length >=5);
+const idadeMaiorQueCinquenta = pessoas.filter(obj => obj.idade >= 50);
+const terminaComA = pessoas.filter(obj => obj.nome.toLowerCase().endsWith('a'));
+console.log(terminaComA);
